67875f2ded8c1fd9237d79de9fbbe12e22e9855e
Wxin96 <18706306187@163.com>
2022-12-07 23:31:12 +0800

    first commit

---
+++ src/main/java/thinking/in/spring/boot/FirstAppByGuiApplication.java
@@ -0,0 +1,22 @@
+package thinking.in.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class FirstAppByGuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAppByGuiApplication.class, args);
+	}
+
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+
+}
---
+++ src/test/java/thinking/in/spring/boot/FirstAppByGuiApplicationTests.java
@@ -0,0 +1,13 @@
+package thinking.in.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAppByGuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
